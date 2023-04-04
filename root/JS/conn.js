7864f74ef8618d979bb351fb93a37f02b9c56930
adi <adityachandrol2002@gmail.com>
2023-04-04 21:29:19 +0530

    Main

--- root/JS/conn.js
+++ root/JS/conn.js
@@ -8,15 +8,35 @@
 //     console.log("Connected to XAMPP Server!");
 //   });
 
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection({
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "8520",
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+var mysql_package = require('mysql');
+// Create the connection using the server,username and password.
+//In my scenario - server is the localhost,
+//username is root,
+//password is empty.
+var connection_data = mysql_package.createConnection({
   host: "localhost",
   user: "root",
-  password: "8520",
+  password: ""
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+connection_data.connect(function(err) {
+  console.log("Connected to XAMPP Server!");
+//sql query to create a database named  facility in XAMPP
+  connection_data.query("CREATE DATABASE facility", function (err, result) {
+//Display message in our console.
+    console.log("Database-facility is created");
+  });
+});   
